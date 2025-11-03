f72d120536ad6f7ea8de63be7b89c2801603e68d
Asaad Saad <asaadsaad@gmail.com>
2025-11-02 19:54:46 -0600

    Add files via upload

---
+++ linkedSignal exercise/app/app.component.ts
@@ -0,0 +1,75 @@
+import { JsonPipe } from "@angular/common";
+import { Component, linkedSignal, signal } from "@angular/core";
+
+const PRODUCTS: string[] = [
+  "Apple",
+  "Banana",
+  "Cherry",
+  "Date",
+  "Elderberry",
+  "Fig",
+  "Grape",
+  "Honeydew",
+  "Indian Prune",
+  "Jackfruit",
+  "Kiwi",
+  "Lemon",
+  "Mango",
+];
+
+@Component({
+  selector: "app-root",
+  imports: [JsonPipe],
+  template: `
+  <h2>Inventory</h2>
+  <button (click)="addProduct()">Add Product</button>
+  <button (click)="removeProduct()">Remove Product</button>
+  <pre>
+    {{ products() | json }}
+  </pre>
+
+
+  <h2>Selected: {{selectedProduct()}}</h2>
+  <button (click)="nextProduct()">Next Product</button>
+  <button (click)="prevProduct()">Prev Product</button>
+`,
+})
+export class AppComponent {
+  readonly products = signal(["Apple", "Banana", "Cherry"]);
+
+  readonly selectedProduct = signal("Apple");
+
+  /* 1. Create a simple linked signal that sets the selected product to the first
+        product in the list., wheven the inventory changes */
+
+  /* 2. Change the `linkedSignal` so you use the second signature, supply an object
+        with source and computation properties */
+
+  /* 3. In the computation, use the previous value, to check if the selected product
+        is still in the list, if not, set the selected product to the first product in the list */
+
+  addProduct() {
+    this.products.update((prods) => [...prods, PRODUCTS[prods.length]]);
+  }
+
+  removeProduct() {
+    this.products.update((prods) => prods.slice(0, -1));
+  }
+
+  nextProduct() {
+    this.selectedProduct.update((selected) => {
+      const index = this.products().indexOf(selected);
+      return this.products()[(index + 1) % this.products().length];
+    });
+  }
+
+  prevProduct() {
+    this.selectedProduct.update((selected) => {
+      const index = this.products().indexOf(selected);
+      return this
+        .products()[
+          (index - 1 + this.products().length) % this.products().length
+        ];
+    });
+  }
+}
